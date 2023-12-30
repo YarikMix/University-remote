5b0095d5e679040fcd65da7556a8533d31706bcd
YarikMix <43493788+YarikMix@users.noreply.github.com>
2023-12-31 00:03:29 +0300

    Clean

--- internal/models/audienceRequest.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type AudienceRequest struct {
-	AccessToken int64  `json:"access_token"`
-	Audience    string `json:"audience"`
-}
--- internal/models/request.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Request struct {
-	AccessToken int64 `json:"access_token"`
-	LessonId    int64 `json:"lesson_id"`
-}
